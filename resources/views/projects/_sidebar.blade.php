447ad189f4556b48c378c8402c95be6282fd3279
Dee <dee.lee@jotangi.com>
2023-06-29 14:58:05 +0800

    sidebar

---
+++ resources/views/projects/_sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <!-- 侧边栏内容 -->
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">連結1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">連結2</a>
+        </li>
+        <!-- 其他侧边栏菜单项 -->
+    </ul>
+</div>
